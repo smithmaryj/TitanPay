5b0dd91e921c7e10b12b16b4452676b614dfcd96
maryJ <smithmaryj@pcsb.org>
2016-06-06 20:43:02 -0400

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,27 +1,36 @@
-class Employee:
-
-    def __init__(self):
-        self.__last_name = " "
-        self.__first_name = " "
-
-    def get_last_name(self):
-        return self.__last_name
-
-    def get_first_name(self):
-        return self.__first_name
-
-    def set_last_name(self, last_name):
+class Employees:
+    def __init__(self, employee_id, last_name, first_name, weekly_dues):
+        self.__employee_id = employee_id
         self.__last_name = last_name
-
-    def set_first_name(self, first_name):
         self.__first_name = first_name
+        self.__weekly_dues = weekly_dues
 
-if __name__ == "__main__":
-    employee_last_name = Employee()
-    employee_first_name = Employee()
+    def get_employee_id(self):
+        return self.__employee_id
 
-    employee_last_name.set_last_name("Tillman,")
-    employee_first_name.set_first_name("Adrian")
-    print("Employee Full Name: " , employee_last_name.get_last_name() , employee_first_name.get_first_name())
+    def get_full_name(self):
+        return self.__last_name + " " + self.__first_name
 
+    def get_weekly_dues(self):
+        return self.__weekly_dues
+
+
+class HourlyEmployee(Employees):
+
+    def __init__(self, employee_id, last_name, first_name, hourly_rate, weekly_dues):
+        Employees.__init__(employee_id, last_name, first_name, weekly_dues)
+        self.__hourly_rate = hourly_rate
+
+
+    def set_hourly_rate(self, hourly_rate):
+        self.__hourly_rate = hourly_rate
+
+    def get_hourly_rate(self, hourly_rate):
+        return self.__hourly_rate
+def main():
+    adrian = HourlyEmployee("Tillman", "Adrian")
+    hourly_rate = "$25/hr"
+
+    print(adrian.get_full_name())
+    print(hourly_rate.get_hourly_rate())
 
