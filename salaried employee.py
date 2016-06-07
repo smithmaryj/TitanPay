958a8971c86531e6c9c3ca91e7ec96e0f0792652
maryJ <smithmaryj@pcsb.org>
2016-06-06 21:06:30 -0400

    Update salaried employee.py

--- salaried employee.py
+++ salaried employee.py
@@ -1,27 +1,49 @@
-class SalariedEmployee:
-
-    def __init__(self):
-        self.__last_name = " "
-        self.__first_name = " "
-
-    def get_last_name(self):
-        return self.__last_name
-
-    def get_first_name(self):
-        return self.__first_name
-
-    def set_last_name(self, last_name):
+class Employees:
+    def __init__(self, employee_id, last_name, first_name, weekly_dues):
+        self.__employee_id = employee_id
         self.__last_name = last_name
-
-    def set_first_name(self, first_name):
         self.__first_name = first_name
+        self.__weekly_dues = weekly_dues
 
-if __name__ == "__main__":
-    salariedemployee_last_name = SalariedEmployee()
-    salariedemployee_first_name = SalariedEmployee()
+    def get_employee_id(self):
+        return self.__employee_id
 
-    salariedemployee_last_name.set_last_name("Tillman,")
-    salariedemployee_first_name.set_first_name("Adrian")
-    print("Salaried Employee Full Name: " , salariedemployee_last_name.get_last_name() , salariedemployee_first_name.get_first_name())
+    def get_full_name(self):
+        return self.__last_name + " " + self.__first_name
+
+    def get_weekly_dues(self):
+        return self.__weekly_dues
+
+
+class SalariedEmployee(Employees):
+
+    def __init__(self, employee_id, last_name, first_name, salary, commission_rate, weekly_dues):
+        Employees.__init__(employee_id, last_name, first_name, weekly_dues)
+        self.__salary = salary
+        self.__commission_rate = commission_rate
+
+    def set_salary(self, salary):
+        self.__salary = salary
+
+    def set_commission_rate(self, commission_rate):
+        self.__commission_rate = commission_rate
+
+    def get_salary(self, salary):
+        return self.__salary
+
+    def get_commission_rate(self, commission_rate):
+        return self.__commission_rate
+
+
+def main():
+    maryj = SalariedEmployee("Smith", "Mary J")
+    salary = "$65,000"
+    commission_rate = "10%"
+
+    print (maryj.get_full_name())
+    print(salary.get_salary())
+    print(commission_rate.get_commission_rate())
+
+main()
 
 
