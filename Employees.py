ff4333f91cf4485a79eb0de10d4a07fc3972b214
smithmaryj <smithmaryj@pcsb.org>
2016-06-08 21:47:57 -0400

    TitanPay

--- Employees.py
+++ Employees.py
@@ -4,6 +4,7 @@
 class Employees:
     # the __init__ method accepts the arguements for general employee information like employee id, full name, and
     # weekly dues to the union services. It initializes the data attributes with these values
+
     def __init__(self, employee_id, last_name, first_name, weekly_dues):
         self.__employee_id = employee_id
         self.__last_name = last_name
@@ -16,8 +17,11 @@ class Employees:
     def set_employee_id(self, employee_id):
         self.__employee_id
 
-    def set_full_name(self, last_name, first_name):
-        self.__last_name + " " + self.__first_name
+    def set_last_name(self, last_name):
+        self.__last_name
+
+    def set_first_name(self, first_name):
+        self.__first_name
 
     def set_weekly_dues(self, weekly_dues):
         self.__weekly_dues
@@ -28,16 +32,18 @@ class Employees:
     def get_employee_id(self):
         return self.__employee_id
 
-    def get_full_name(self):
-        return self.__last_name + " " + self.__first_name
+    def get_last_name(self):
+        return self.__last_name
+
+    def get_first_name(self):
+        return self.__first_name
 
     def get_weekly_dues(self):
         return self.__weekly_dues
 
 
-# The Type of Employee represents that of a hourly paid one;
-# it is a subclass of the Employee class.
 class HourlyEmployee(Employees):
+
     # the __init__ method accepts the arguments for the general employee information and the
     # specific salaried employee information like employee id, full name, and weekly dues to
     # the union services. It initializes the data attributes with these values
@@ -53,7 +59,6 @@ class HourlyEmployee(Employees):
 
     # Mutator methods for the __hourly_rate attribute
 
-    def set_salary(self, salary):
         def set_hourly_rate(self, hourly_rate):
             self.__hourly_rate = hourly_rate
 
@@ -63,17 +68,34 @@ class HourlyEmployee(Employees):
         return self.__hourly_rate
 
 
-def main():
-    # creates an object from the Hourly Employee subclass of the Employee superclass
-    # the employee is Adrian Tillman, his pay rate is $25/hour
+# The Type of Employee represents that of a salaried one;
+# it is a subclass of the Employee class.
 
-    adrian = HourlyEmployee("Tillman", "Adrian", "25", "10", "")
-    hourly_rate = "his salary is $25/hour"
+class SalariedEmployee(Employees):
+    # the __init__ method accepts the arguments for the general employee information and the
+    # specific salaried employee information like employee id, full name, and weekly dues to
+    # the union services. It initializes the data attributes with these values
 
-    # Display the Hourly Employees information
+    def __init__(self, employee_id, last_name, first_name, salary, commission_rate, weekly_dues):
+        # Call the superclass's __init__method and pass the arguments necessary for the
+        # general employee; self is also passed as an argument.
+        Employees.__init__(self, employee_id, last_name, first_name, weekly_dues)
 
-    print(adrian.get_full_name())
-    print(hourly_rate.get_hourly_rate())
+        # inititalizes the __salary and __commission_rate attributes
+        self.__salary = salary
+        self.__commission_rate = commission_rate
 
-    # Call the main function
-main()
+    # Mutator methods for the __salary and __commission_rate attributes
+    def set_salary(self, salary):
+        self.__salary = salary
+
+    def set_commission_rate(self, commission_rate):
+        self.__commission_rate = commission_rate
+
+    # Accessor methods for the __salary and __commission_rate attributes
+
+    def get_salary(self, salary):
+        return self.__salary
+
+    def get_commission_rate(self, commission_rate):
+        return self.__commission_rate
