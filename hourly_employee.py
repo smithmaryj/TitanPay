46f904744c684766cbc669b08293caf80376d69e
maryJ <smithmaryj@pcsb.org>
2016-06-21 02:06:49 -0400

    Update hourly_employee.py

--- hourly_employee.py
+++ hourly_employee.py
@@ -1,40 +1,30 @@
 # Mary J Smith, 2295760
 # The Employees class represents a superclass of general employee information needed for Titan Payroll to process
-
 import time_card
-import Employees
+from employee import Employee 
+from time_card import TimeCard
 
-class ListOfTimecards(time_card.TimeCard):
-    #creates a new TimeCard object from the Timecard class in the
-    #time_card.py file
+class HourlyEmployee(Employee):
+    def __init__(self, hourly_rate):
+        self.hourly_rate = hourly_rate
+
+class ListOfTimeCards(time_card.TimeCard):
+    # creates a new TimeCard object from the TimeCard class in the
+    # time_card.py file
     def __init__(self, clock_in, clock_out, date, time):
         self.clock_in = clock_in
         self.clock_out = clock_out
         self.date = date
         self.time = time
-        timecard = time_card(self, date, time)
+        self.time_card = TimeCard(self, date, time)
         self.__time_card.append(time_card)
 
-def main():
-    # creates an object from the Hourly Employee subclass of the Employee superclass
-    # the employee is Adrian Tillman, his pay rate is $20/hour
+    def time_card(self, date, time):
+        pass
 
-    adrian_HourlyEmployee = Employees.HourlyEmployee('3546255', 'Tillman', 'Adrian', '20', '10')
-
-    # Display the Hourly Employees information
-
-    print('Employee ID:', adrian_HourlyEmployee.get_employee_id())
-    print('Last Name:', adrian_HourlyEmployee.get_last_name())
-    print('First Name:', adrian_HourlyEmployee.get_first_name())
-    print('Weekly Dues:', adrian_HourlyEmployee.get_weekly_dues())
-
-
-    # Call the main function
-
-
-main()
-
-
-
-
+    def execute(self):
+        e = self.get_employee(self.employee_id)
+        pc = e.classification
+        tc = TimeCard(self.date, self.hours)
+        pc.add_time_card(tc)
 
