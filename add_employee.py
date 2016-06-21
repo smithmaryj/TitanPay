a8fcec7be42c035983ef69638cc3195596b8b4b4
maryJ <smithmaryj@pcsb.org>
2016-06-21 13:48:37 -0400

    Update add_employee.py

--- add_employee.py
+++ add_employee.py
@@ -1,4 +1,7 @@
-from employee import Employee
+import employee
+import add_employee
+import salaried_employee
+import hourly_employee
 
 class AddEmployee(object):
     def __init__(self, name, address):
@@ -7,7 +10,26 @@ class AddEmployee(object):
 
 
     def execute(self):
-        self.e = Employee(self.name, self.address)
+        self.e = employee.Employee(self.name, self.address)
         self.make_classification()
 
         return self.e.id
+
+class AddHourlyEmployee(add_employee.AddEmployee):
+    def __init__(self, name, address, hourly_rate):
+        add_employee.AddEmployee.__init__(self, name, address)
+        self.hourly_rate = hourly_rate
+
+    def make_classification(self):
+        self.e.classification = hourly_employee.HourlyEmployee(self.hourly_rate)
+
+
+
+class AddSalariedEmployee(AddEmployee):
+    def __init__(self, name, address, salary, db):
+        add_employee.AddEmployee.__init__(self, name, address, db)
+        self.salary = salary
+
+    def make_classification(self):
+        self.e.classification = salaried_employee.SalariedEmployee(self.salary)
+
